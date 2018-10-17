7513a15c49d3cbc0b782afa20a8139dbe525a384
Cody Konior <cody@codykonior.com>
2018-10-17 23:32:48 +0800

    Fix MAC

--- Public/New-OftenOnLab.ps1
+++ Public/New-OftenOnLab.ps1
@@ -3,8 +3,15 @@ function New-OftenOnLab {
     param(
     )
 
-    OftenOn -ConfigurationData (Get-OftenOnLabConfiguration) -OutputPath "$PSScriptRoot\..\MOF"
+    <#
+        It's important the configuration data is only retrieved once here because it contains some
+        random MAC addresses, and these must be the same between compiling the MOF and starting the
+        lab configuration (as Lability will use those to create the VMs and can't read it from the
+        MOF)
+    #>
+    $configurationData = Get-OftenOnLabConfiguration
+    OftenOn -ConfigurationData $configurationData -OutputPath "$PSScriptRoot\..\MOF"
 
     $administrator = New-Object System.Management.Automation.PSCredential('Administrator', ('Admin2018!' | ConvertTo-SecureString -AsPlainText -Force))
-    Start-LabConfiguration -ConfigurationData (Get-OftenOnLabConfiguration) -IgnorePendingReboot -Credential $administrator -NoSnapshot -Path "$PSScriptRoot\..\MOF"
+    Start-LabConfiguration -ConfigurationData $configurationData -IgnorePendingReboot -Credential $administrator -NoSnapshot -Path "$PSScriptRoot\..\MOF"
 }
