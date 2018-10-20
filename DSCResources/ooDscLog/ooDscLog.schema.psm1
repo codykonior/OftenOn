14042607c7277c8336b3b224e32246e40bfe5755
Cody Konior <cody@codykonior.com>
2018-10-20 15:51:11 +0800

    Fix log changes

--- DSCResources/ooDscLog/ooDscLog.schema.psm1
+++ DSCResources/ooDscLog/ooDscLog.schema.psm1
@@ -1,17 +1,21 @@
 Configuration ooDscLog {
     # 1 hour is about 1MB so this gives you two days worth
+    # LogMode cannot be changed to Cicular on these unfortunately
+
+    xWinEventLog "AlterDSCOperationalLog" {
+        LogName = "Microsoft-Windows-DSC/Operational"
+        MaximumSizeInBytes = 48MB
+    }
 
     xWinEventLog "EnableDSCAnalyticLog" {
         LogName = "Microsoft-Windows-DSC/Analytic"
         MaximumSizeInBytes = 48MB
-        LogMode = 'Circular'
         IsEnabled = $true
     }
 
     xWinEventLog "EnableDSCDebugLog" {
         LogName = "Microsoft-Windows-DSC/Debug"
         MaximumSizeInBytes = 48MB
-        LogMode = 'Circular'
         IsEnabled = $true
     }
 }
