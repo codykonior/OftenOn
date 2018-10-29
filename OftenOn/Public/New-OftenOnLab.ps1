7088417f5aa26ed0f38ac5205d8f6543d0743bf3
Cody Konior <cody@codykonior.com>
2018-10-29 15:31:04 +0800

    Do first time Lability setup if required

--- OftenOn/Public/New-OftenOnLab.ps1
+++ OftenOn/Public/New-OftenOnLab.ps1
@@ -16,6 +16,12 @@ function New-OftenOnLab {
     OftenOn -ConfigurationData $configurationData -OutputPath "$PSScriptRoot\..\MOF"
 
     if (!$SkipConfiguration) {
+        if (!(Test-LabHostConfiguration -IgnorePendingReboot)) {
+            "This is the first run, executing Start-LabHostCpnfiguration to enable Lability prerequisites which are installed to C:\Lability"
+            Start-LabHostConfiguration -IgnorePendingReboot
+            "If you see errors after this, try rebooting"
+        }
+
         $administrator = New-Object System.Management.Automation.PSCredential('Administrator', ('Admin2018!' | ConvertTo-SecureString -AsPlainText -Force))
         Start-LabConfiguration -ConfigurationData $configurationData -IgnorePendingReboot -Credential $administrator -NoSnapshot -Path "$PSScriptRoot\..\MOF"
 
