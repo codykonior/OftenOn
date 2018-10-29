99a5e335684750301270775b3581779cfb74a94e
Cody Konior <cody@codykonior.com>
2018-10-29 15:55:20 +0800

    Reduce error messages caused by DSC

--- OftenOn/Public/New-OftenOnLab.ps1
+++ OftenOn/Public/New-OftenOnLab.ps1
@@ -7,6 +7,22 @@ function New-OftenOnLab {
     )
 
     <#
+        If these registry keys don't exist then PSDesiredStateConfiguration spams the $Error
+        variable with hundreds of SilentlyContinue errors. So instead I set them to the safe
+        default value if they don't exist - that way $Error will be clean and it's easier to
+        pick up any real errors.
+    #>
+    if (!(Test-Path 'HKLM:\SOFTWARE\Microsoft\PowerShell\3\DSC')) {
+        New-Item -Path 'HKLM:\SOFTWARE\Microsoft\PowerShell\3\DSC' -ItemType Directory | Out-Null
+    }
+    if (!(Get-Item 'HKLM:\SOFTWARE\Microsoft\PowerShell\3\DSC' | Where-Object { $_.Property -eq 'PSDscAllowDomainUser' })) {
+        New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\PowerShell\3\DSC' -Name 'PSDscAllowDomainUser' -Value "False" | Out-Null
+    }
+    if (!(Get-Item 'HKLM:\SOFTWARE\Microsoft\PowerShell\3\DSC' | Where-Object { $_.Property -eq 'PSDscAllowPlainTextPassword' })) {
+        New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\PowerShell\3\DSC' -Name 'PSDscAllowPlainTextPassword' -Value "False" | Out-Null
+    }
+
+    <#
         It's important the configuration data is only retrieved once here because it contains some
         random MAC addresses, and these must be the same between compiling the MOF and starting the
         lab configuration (as Lability will use those to create the VMs and can't read it from the
